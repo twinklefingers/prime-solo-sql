e6810f022fd426fc2b1a9d220bb9daaa1f1429b7
twinklefingers <B1-66-ER@Rays-MBP.hsd1.mn.comcast.net>
2016-08-15 22:55:34 -0500

    finished postgre sql syntax_practice

--- solutions.sql
+++ solutions.sql
@@ -30,16 +30,17 @@ UPDATE syntax_practice SET account_balance = 10 WHERE account_balance = 0 AND tr
 SELECT * FROM syntax_practice WHERE transactions_attempted >= 9;
 
 --Get the username and account balance of the 3 users with the highest balances, sort highest to lowest balance. NOTE: Research LIMIT
-
+SELECT * FROM syntax_practice ORDER BY account_balance DESC LIMIT 3;
 
 --Get the username and account balance of the 3 users with the lowest balances, sort lowest to highest balance.
-
+SELECT * FROM syntax_practice ORDER BY account_balance ASC LIMIT 3;
 
 --Get all users with account balances that are more than $100.
 SELECT * FROM syntax_practice WHERE account_balance > 100;
 
 --Add a new record.
 INSERT INTO syntax_practice ("username", "city", "transactions_completed", "transactions_attempted", "account_balance")
-VALUES ('twinklefingers', 'las vegas', '1', '1', '1000000');
+VALUES ('ray', 'las vegas', '1', '1', '1000000');
 
---The bank is losing money in Miami and Phoenix and needs to unload low transaction customers: Delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
+--Delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
+DELETE FROM syntax_practice WHERE city = 'miami' OR city = 'phoenix' AND transactions_completed < 5;
